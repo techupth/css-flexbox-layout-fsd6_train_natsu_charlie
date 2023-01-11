f59bf66d3a817ce831f35a3846da495ff172674c
Jettapat <jettapat@techupth.com>
2023-01-11 17:03:57 +0700

    fix: optimize test case ex-2

--- tests/ex-2.spec.js
+++ tests/ex-2.spec.js
@@ -6,16 +6,16 @@ test('ex-2-test', async ({ page }) => {
   const currentPath = path.join(process.cwd(), 'ex2-holygrail-layout/index.html');
   const url = "file:///" + currentPath
   await page.setViewportSize({
-    width: 3584,
-    height: 2154
+    width: 1920,
+    height: 1080
   })
   await page.goto(url)
   expect(await page.screenshot({
     fullPage: true,
   })).toMatchSnapshot({
-    threshold: 0.7,
-    maxDiffPixels: 250000,
-    maxDiffPixelRatio: 0.2,
+    threshold: 0.2,
+    // maxDiffPixels: 10000,
+    maxDiffPixelRatio: 0.02,
   },
   );
 });
